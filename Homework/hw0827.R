d153bdeea8d9c9860b867edb7c43a021a9a03985
Collin Dougherty <91863638+collindougherty@users.noreply.github.com>
2022-09-11 20:41:03 -0500

    SPMC files

---
+++ Homework/hw0827.R
@@ -0,0 +1,4 @@
+# I am going to add the numbers 5 and 7 to get 12
+# I will store the answer to the question in variable addition_answer
+addition_answer = 5 + 7
+print(addition_answer)
---
+++ Homework/spmc0827.R
@@ -0,0 +1,5 @@
+# I am going to add the numbers 5 and 7 to get 12
+# I will store the answer to the question in variable addition_answer
+addition_answer = 5 + 7
+print(addition_answer)
+test test
